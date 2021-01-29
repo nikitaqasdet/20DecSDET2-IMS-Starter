56e39bfaa86e81a654ca965b27a2a800e00d4853
nikita-dean <ndean@qa.com>
2021-01-29 11:35:38 +0000

    updated databases, commented out OrderDao (for now), improved coverage

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -2,24 +2,33 @@ drop schema ims;
 
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
-USE `ims` ;
+USE `ims`;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `cust_id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`cust_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
     `item_name` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `price` DEC(3,2) NOT NULL,
+    PRIMARY KEY (`item_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    fk_customer_id INT NOT NULL,
+    `ord_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `fk_cust_id` INT NOT NULL,
     `order_value` INT(40) DEFAULT NULL,
-    FOREIGN KEY (fk_customer_id) REFERENCES sustomers(customer_id)
+    PRIMARY KEY (`ord_id`)
+    FOREIGN KEY (`fk_cust_id`) REFERENCES customers(`cust_id`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS `ims`.`orderline` (
+	`fk_ord_id` INT(11) NOT NULL,
+    `fk_item_id` INT(11) NOT NULL,
+    FOREIGN KEY (`fk_ord_id`) REFERENCES `orders`(`ord_id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`item_id`) ON UPDATE CASCADE ON DELETE CASCADE,
 );
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,34 @@
-DROP TABLE IF EXISTS `customers`;
+drop schema ims;
 
-CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE SCHEMA IF NOT EXISTS `ims`;
+
+USE `ims`;
+
+CREATE TABLE IF NOT EXISTS `ims`.`customers` (
+    `cust_id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`cust_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `price` DEC(3,2) NOT NULL,
+    PRIMARY KEY (`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `ord_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `fk_cust_id` INT NOT NULL,
+    `order_value` INT(40) DEFAULT NULL,
+    PRIMARY KEY (`ord_id`)
+    FOREIGN KEY (`fk_cust_id`) REFERENCES customers(`cust_id`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS `ims`.`orderline` (
+	`fk_ord_id` INT(11) NOT NULL,
+    `fk_item_id` INT(11) NOT NULL,
+    FOREIGN KEY (`fk_ord_id`) REFERENCES `orders`(`ord_id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`item_id`) ON UPDATE CASCADE ON DELETE CASCADE,
 );
